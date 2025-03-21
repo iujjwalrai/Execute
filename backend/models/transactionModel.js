f3ea7260065439fc6bde4cc83e03b0c129a3fc95
yashag710 <yashag1810@gmail.com>
2025-03-21 14:20:50 +0530

    Model Changes

---
+++ backend/models/transactionModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = mongoose.Schema({
+    transaction_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    failed_attempts: {
+        type: Number,
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    payment_mode: {
+        type: String,
+        required: true
+    },
+    payment_channel: {
+        type: String,
+        required: true
+    },
+    payment: {
+        type: String,
+        required: true,
+        enum: ['pending', 'completed', 'failed']
+    },
+    payee_id: {
+        type: String,
+        required: true,
+        ref: 'Payee'
+    },
+    payer_id: {
+        type: String,
+        required: true,
+        ref: 'Payer'
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    ip: {
+        type: String,
+        required: true
+    },
+    state : {
+        type : String,
+        required : true
+    }
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
